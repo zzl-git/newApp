310b40e5975c63d2148aa9b3636b5a1350c56f35
mygit <784778060@qq.com>
2019-05-02 12:47:27 +0800

    首页完成

--- js/shouye.js
+++ js/shouye.js
@@ -1,10 +1,28 @@
-
-var qiehuan = document.getElementById('qiehuan');
-var qiehuan_box = document.getElementsByClassName('qiehuan_box')[0];
-qiehuan.onclick= function(){
-	if (qiehuan_box.style.display=='block') {
-		qiehuan_box.style.display='none';
-	} else{
-		qiehuan_box.style.display='block';
+var num = 0;
+$('#qiehuan').click(function() {
+	if (num == 0) {
+		$('.qiehuan_box').show(100);
+		num = 1;
+		$(this).addClass('xuanzhuan')
+		// console.log($(this).class)
+	} else {
+		$('.qiehuan_box').hide();
+		num = 0;
+		$(this).removeClass("xuanzhuan")
+		// $(this).removeClass('xuanzhuan')
 	}
-}
+
+})
+console.log($('allBox'));
+$('.allBox').mouseover(function() {
+	$('.allBox').click(function() {
+		$('.allBox_main').show();
+	})
+})
+$('.allBox_main').mouseenter(function() {
+	console.log('444')
+	$('body').click(function() {
+		console.log('555')
+		$('.allBox_main').hide();
+	})
+})
